e2fe90a14acf0588006d815dc9835504ec9dccc3
Uilian Souza <uilian@gmail.com>
2017-09-23 10:42:13 -0300

    Initial commit. Using an strategy of both iOS and Android sharing same code base.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Auth-React
+React-Native Course Project second project.
+
+[The Complete React Native and Redux Course](https://www.udemy.com/the-complete-react-native-and-redux-course/)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Some text from App!
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
