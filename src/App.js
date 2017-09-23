e0b1aad26b0624d69e0119ddaa350e12a5f335f7
Uilian Souza <uilian@gmail.com>
2017-09-23 11:10:11 -0300

    Adding reusable components from previous project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import { Header } from './components/common';
 class App extends Component {
   render() {
     return (
       <View>
+        <Header headerText="This is a header" />
         <Text>
           Some text from App!
         </Text>
