c6149dc542952c10857b54c20e02197e8dda18a6
Uilian Souza <uilian@gmail.com>
2017-09-23 18:40:08 -0300

    Refactored the log in flow. Added a spinner component.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,25 +6,55 @@ import { Button, Card, CardSection, Input, Spinner } from './common';
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: '',
+    loading: false
   };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    this.setState({ error: '' });
+    this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            this.setState({ error: 'Authentication Failed.' });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFailure.bind(this));
       });
   }
 
-  render() {
+  onLoginFailure() {
+    this.setState({
+      error: 'Authentication Failed.',
+      loading: false });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      error: '',
+      email: '',
+      password: '',
+      loading: false
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return (
+        <Spinner size='small' />
+      );
+    }
     return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
+  }
+
+  render() {
+     return (
       <Card>
         <CardSection>
           <Input
@@ -50,9 +80,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
