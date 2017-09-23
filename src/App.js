722a1291cb502f2ae977324f6f93eeedd8fd022f
Uilian Souza <uilian@gmail.com>
2017-09-23 11:31:54 -0300

    Added Firebase support. Copy configuration from google's project console https://console.firebase.google.com/project/

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyB30rUv0P2yxKj358sl-Scn8pAozlQ5VJA',
+      authDomain: 'auth-react-77e1a.firebaseapp.com',
+      databaseURL: 'https://auth-react-77e1a.firebaseio.com',
+      projectId: 'auth-react-77e1a',
+      storageBucket: 'auth-react-77e1a.appspot.com',
+      messagingSenderId: '536366310093'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
