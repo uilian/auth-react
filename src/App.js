d04481cf8d444acae4e35d74bddc616c63148ed4
Uilian Souza <uilian@gmail.com>
2017-09-23 11:50:27 -0300

    Created login form component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -20,9 +21,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="This is a header" />
-        <Text>
-          Some text from App!
-        </Text>
+        <LoginForm />
       </View>
     );
   }
